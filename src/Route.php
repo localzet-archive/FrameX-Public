cd6e9638caec5db7f0a4465bc325d73b5e650c06
Иван Зорин <local.zet@yandex.ru>
2022-08-11 04:15:28 +0500

    Update Route.php

--- src/Route.php
+++ src/Route.php
@@ -356,6 +356,50 @@ class Route
             // Устанавливаем коллектор в $_collector
             Route::setCollector($route);
 
+            $dir_iterator = new \RecursiveDirectoryIterator(app_path());
+            $iterator = new \RecursiveIteratorIterator($dir_iterator);
+            foreach ($iterator as $file) {
+                if (is_dir($file) || $file->getExtension() != 'php') {
+                    continue;
+                }
+
+                $file_path = str_replace('\\', '/', $file->getPathname());
+                if (strpos($file_path, 'controller') === false) {
+                    continue;
+                }
+
+                $uri_path = strtolower(str_replace('controller/', '', substr(substr($file_path, strlen(app_path())), 0, -4)));
+                $class_name = str_replace('/', '\\', substr(substr($file_path, strlen(base_path())), 0, -4));
+
+                if (!class_exists($class_name)) {
+                    echo "Class $class_name not found, skip route for it\n";
+                    continue;
+                }
+
+                $class = new \ReflectionClass($class_name);
+                $methods = $class->getMethods(\ReflectionMethod::IS_PUBLIC);
+
+                $route = function ($uri, $cb) {
+                    //echo "Route $uri [{$cb[0]}, {$cb[1]}]\n";
+                    Route::any($uri, $cb);
+                    Route::any($uri . '/', $cb);
+                };
+
+                foreach ($methods as $item) {
+                    $action = $item->name;
+                    if (in_array($action, ['__construct', '__destruct'])) {
+                        continue;
+                    }
+                    if ($action === 'index') {
+                        if (substr($uri_path, -6) === '/index') {
+                            $route(substr($uri_path, 0, -6), [$class_name, $action]);
+                        }
+                        $route($uri_path, [$class_name, $action]);
+                    }
+                    $route($uri_path . '/' . $action, [$class_name, $action]);
+                }
+            }
+
             // Роутер из глобальной конфигурации
             $route_config_file = $config_path . '/route.php';
             if (\is_file($route_config_file)) {
