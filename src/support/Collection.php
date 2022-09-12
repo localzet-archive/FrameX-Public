27c370502843065a704a98a5dde51d4f438e9fbb
Ivan Zorin <creator@localzet.ru>
2022-09-13 00:24:02 +0300

    New Data Classes

---
+++ src/support/Collection.php
@@ -0,0 +1,165 @@
+<?php
+
+/**
+ * @package     FrameX (FX) Engine
+ * @link        https://localzet.gitbook.io
+ * 
+ * @author      localzet <creator@localzet.ru>
+ * 
+ * @copyright   Copyright (c) 2018-2020 Zorin Projects 
+ * @copyright   Copyright (c) 2020-2022 NONA Team
+ * 
+ * @license     https://www.localzet.ru/license GNU GPLv3 License
+ */
+
+namespace support;
+
+/**
+ * Простой сборщик данных.
+ */
+final class Collection
+{
+    /**
+     * Данные
+     *
+     * @var mixed
+     */
+    protected $collection = null;
+
+    /**
+     * @param mixed $data
+     */
+    public function __construct($data = null)
+    {
+        $this->collection = new \stdClass();
+
+        if (is_object($data)) {
+            $this->collection = $data;
+        }
+
+        $this->collection = (object)$data;
+    }
+
+    /**
+     * Извлекает всю коллекцию в виде массива
+     *
+     * @return mixed
+     */
+    public function toArray()
+    {
+        return (array)$this->collection;
+    }
+
+    /**
+     * Извлекает элемент
+     *
+     * @param $property
+     *
+     * @return mixed
+     */
+    public function get($property)
+    {
+        if ($this->exists($property)) {
+            return $this->collection->$property;
+        }
+
+        return null;
+    }
+
+    /**
+     * Добавить или обновить элемент
+     *
+     * @param $property
+     * @param mixed $value
+     */
+    public function set($property, $value)
+    {
+        if ($property) {
+            $this->collection->$property = $value;
+        }
+    }
+
+    /**
+     * @param $property
+     *
+     * @return Collection
+     */
+    public function filter($property)
+    {
+        if ($this->exists($property)) {
+            $data = $this->get($property);
+
+            if (!is_a($data, 'Collection')) {
+                $data = new Collection($data);
+            }
+
+            return $data;
+        }
+
+        return new Collection([]);
+    }
+
+    /**
+     * Проверяет, есть ли элемент в коллекции
+     *
+     * @param $property
+     *
+     * @return bool
+     */
+    public function exists($property)
+    {
+        return property_exists($this->collection, $property);
+    }
+
+    /**
+     * Определяет, пуста ли коллекция
+     *
+     * @return bool
+     */
+    public function isEmpty()
+    {
+        return !(bool)$this->count();
+    }
+
+    /**
+     * Подсчитать все предметы в коллекции
+     *
+     * @return int
+     */
+    public function count()
+    {
+        return count($this->properties());
+    }
+
+    /**
+     * Возвращает имена всех свойств элементов
+     *
+     * @return array
+     */
+    public function properties()
+    {
+        $properties = [];
+
+        foreach ($this->collection as $key => $value) {
+            $properties[] = $key;
+        }
+
+        return $properties;
+    }
+
+    /**
+     * Возвращает все значения элементов
+     *
+     * @return array
+     */
+    public function values()
+    {
+        $values = [];
+
+        foreach ($this->collection as $value) {
+            $values[] = $value;
+        }
+
+        return $values;
+    }
+}
---
+++ src/support/Parser.php
@@ -0,0 +1,127 @@
+<?php
+
+/**
+ * @package     FrameX (FX) Engine
+ * @link        https://localzet.gitbook.io
+ * 
+ * @author      localzet <creator@localzet.ru>
+ * 
+ * @copyright   Copyright (c) 2018-2020 Zorin Projects 
+ * @copyright   Copyright (c) 2020-2022 NONA Team
+ * 
+ * @license     https://www.localzet.ru/license GNU GPLv3 License
+ */
+
+namespace support;
+
+/**
+ * Parser
+ *
+ * Этот класс используется для разбора простого текста на объекты. 
+ * Он используется адаптерами OAuth для преобразования ответов API провайдеров в более «управляемый» формат.
+ * 
+ */
+final class Parser
+{
+    /**
+     * Декодирует строку в объект.
+     *
+     * Этот метод сначала попытается проанализировать данные 
+     * как строку JSON (поскольку большинство провайдеров используют этот формат), а затем XML и parse_str.
+     *
+     * @param string $raw
+     *
+     * @return mixed
+     */
+    public function parse($raw = null)
+    {
+        $data = $this->parseJson($raw);
+
+        if (!$data) {
+            $data = $this->parseXml($raw);
+
+            if (!$data) {
+                $data = $this->parseQueryString($raw);
+            }
+        }
+
+        return $data;
+    }
+
+    /**
+     * Декодирует строку JSON
+     *
+     * @param $result
+     *
+     * @return mixed
+     */
+    public function parseJson($result)
+    {
+        return json_decode($result);
+    }
+
+    /**
+     * Декодирует строку XML
+     *
+     * @param $result
+     *
+     * @return mixed
+     */
+    public function parseXml($result)
+    {
+        libxml_use_internal_errors(true);
+
+        $result = preg_replace('/([<\/])([a-z0-9-]+):/i', '$1', $result);
+        $xml = simplexml_load_string($result);
+
+        libxml_use_internal_errors(false);
+
+        if (!$xml) {
+            return [];
+        }
+
+        $arr = json_decode(json_encode((array)$xml), true);
+        $arr = array($xml->getName() => $arr);
+
+        return $arr;
+    }
+
+    /**
+     * Разбирает строку на переменные
+     *
+     * @param $result
+     *
+     * @return \StdClass
+     */
+    public function parseQueryString($result)
+    {
+        parse_str($result, $output);
+
+        if (!is_array($output)) {
+            return $result;
+        }
+
+        $result = new \StdClass();
+
+        foreach ($output as $k => $v) {
+            $result->$k = $v;
+        }
+
+        return $result;
+    }
+
+    /**
+     * Нужно улучшить
+     *
+     * @param $birthday
+     * @param $seperator
+     *
+     * @return array
+     */
+    public function parseBirthday($birthday, $seperator)
+    {
+        $birthday = date_parse($birthday);
+
+        return [$birthday['year'], $birthday['month'], $birthday['day']];
+    }
+}
