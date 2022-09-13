7e601832acb53b3ad7f447f1d88417e355955a0f
Ivan Zorin <creator@localzet.ru>
2022-09-13 08:19:14 +0300

    Fix

--- src/support/Storage.php
+++ src/support/Storage.php
@@ -1,17 +1,23 @@
 <?php
 
-/**********************************************************
- *  @author Ivan Zorin <creator@localzet.ru>              *
- *  @license GNU General Public License v3.0              *
- *  @copyright Zorin Projects <www.localzet.ru>           *
- **********************************************************/
+/**
+ * @package     FrameX (FX) Engine
+ * @link        https://localzet.gitbook.io
+ * 
+ * @author      localzet <creator@localzet.ru>
+ * 
+ * @copyright   Copyright (c) 2018-2020 Zorin Projects 
+ * @copyright   Copyright (c) 2020-2022 NONA Team
+ * 
+ * @license     https://www.localzet.ru/license GNU GPLv3 License
+ */
 
-namespace LIS\Storage;
+namespace support;
 
 /**
  * Session storage manager
  */
-class Session
+class Storage
 {
     /**
      * Namespace
