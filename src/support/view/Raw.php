11b82fd280dbcb97d2a564276d070e33cc7ce1c4
Ivan Zorin <creator@localzet.ru>
2022-12-04 11:42:43 +0300

    Update Raw.php

--- src/support/view/Raw.php
+++ src/support/view/Raw.php
@@ -54,9 +54,9 @@ class Raw implements View
         $view_footer = \config("{$config_prefix}view.options.view_footer", "footer");
         $app = $app === null ? $request->app : $app;
         $base_view_path = $plugin ? \base_path() . "/plugin/$plugin/app" : \app_path();
-        $__template_head__ = $app === '' || $view_global ? "$base_view_path/view/$view_head.$view_suffix" : "$base_view_path/$app/view/$view_head.$view_suffix";
-        $__template_body__ = $app === '' || $view_global ? "$base_view_path/view/$template.$view_suffix" : "$base_view_path/$app/view/$template.$view_suffix";
-        $__template_foot__ = $app === '' || $view_global ? "$base_view_path/view/$view_footer.$view_suffix" : "$base_view_path/$app/view/$view_footer.$view_suffix";
+        $__template_body__ = $app === '' ? "$base_view_path/view/$template.$view_suffix" : "$base_view_path/$app/view/$template.$view_suffix";
+        $__template_head__ = ($view_global == true ? \app_path() : $base_view_path) . ($app === '' ? "/view/$view_head.$view_suffix" : "/$app/view/$view_head.$view_suffix");
+        $__template_foot__ = ($view_global == true ? \app_path() : $base_view_path) . ($app === '' ? "/view/$view_footer.$view_suffix" : "/$app/view/$view_footer.$view_suffix");
 
         \extract(static::$_vars);
         \extract($vars);
