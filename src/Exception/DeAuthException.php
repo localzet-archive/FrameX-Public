805b495178e4f1d397085d493be5e4ac706a87a8
Ivan Zorin <creator@localzet.ru>
2022-12-08 12:56:33 +0300

    Create DeAuthException.php

---
+++ src/Exception/DeAuthException.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * @package     Localzet Development Kit
+ * @link        https://localzet.gitbook.io/
+ * 
+ * @author      Ivan Zorin (localzet) <creator@localzet.ru>
+ * @copyright   Copyright (c) 2018-2022 Localzet Group
+ * @license     https://www.localzet.ru/license GNU GPLv3 License
+ */
+
+declare(strict_types=1);
+
+namespace localzet\FrameX\Exception;
+
+class DeAuthException extends \RuntimeException
+{
+}
